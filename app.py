4b61f7f50bb60f01c4ab17ea7405882523e44743
森本帆南 <morimotohannna@morimotohonaminoMacBook-Air.local>
2025-05-19 08:19:55 +0900

    最新のコードに更新

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ import datetime
 import random
 import uuid
 #from dotenv import load_dotenv
-#from openai import OpenAI
 import openai
 from supabase_helper import supabase
 
 # -------------------- 初期設定 --------------------
 st.set_page_config(page_title="ポジティブ習慣アプリ", page_icon="🌟")
 #load_dotenv()
-#client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 openai.api_key = os.getenv("OPENROUTER_API_KEY")
 openai.base_url = "https://openrouter.ai/api/v1"
 
@@ -44,6 +42,7 @@ def get_gpt_reply(entry, goals):
 この出来事に対して、温かく励ますような言葉や労いを自然な文章で1〜2文で返してください。
 また、出来事がユーザーの目標に関連していた時には、それに気づいてあげてください。
 """
+
     response = openai.ChatCompletion.create(
     model="openai/gpt-3.5-turbo",  # openrouterはモデル名に `openai/` をつける
     messages=[{"role": "user", "content": prompt}],
