deede3a3717588b7136450d91fcc88e414c83016
森本帆南 <morimotohannna@morimotohonaminoMacBook-Air.local>
2025-05-19 08:45:38 +0900

    最新の コードに更新

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from supabase_helper import supabase
 # -------------------- 初期設定 --------------------
 st.set_page_config(page_title="ポジティブ習慣アプリ", page_icon="🌟")
 #load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENROUTER_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENROUTER_API_KEY"),base_url="https://openrouter.ai/api/v1")
 
 # -------------------- ユーザー識別 --------------------
 if "user_id" not in st.session_state:
